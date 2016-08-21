f16e49b9b5a13cb6111227c4a5090efcbfc22546
HJ.Park <hyunjun19@gmail.com>
2016-08-21 11:16:00 +0900

    user-request-log schema update

--- longo-app/models/user-request-log.js
+++ longo-app/models/user-request-log.js
@@ -1,11 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
+
 var userRequestLogSchema = new Schema({
-  level: String,
-  datetime: Number,
-  message: String,
-  createdAt: Date
+    host: String,
+    uri: String,
+    method: String,
+    queryParameter: String,
+    status: String,
+    os: String,
+    device: String,
+    browser: String,
+    browserVersion: String,
+    headerMap: Schema.Types.Mixed,
+    parameterMap: Schema.Types.Mixed,
+    userId: Number,
+    requestElapsedTime: Number,
+    requestDateTime: String,
+    sessionKey: String,
+    referer: String,
+    createdAt: Date
 });
 
 var UserRequestLog = mongoose.model('user-request-log', userRequestLogSchema);
--- longo-app/routes/logger.js
+++ longo-app/routes/logger.js
@@ -10,14 +10,23 @@ router.post('/:buket', function(req, res, next) {
     res.header('x-response-status', 'OK');
     res.sendStatus(204);
 
-    var userRequestLog = new UserRequestLog(req.body);
-    userRequestLog.createdAt = new Date();
-
-    userRequestLog.save(function(err) {
-        if (err) throw err;
-        console.log('user-request-logs saved successfully!');
-    });
+    saveBuket(req);
 });
 
+function saveBuket(req) {
+    switch (req.params.buket) {
+        case 'user-request-log':
+            var userRequestLog = new UserRequestLog(req.body);
+            userRequestLog.createdAt = new Date();
+            userRequestLog.save(function(err) {
+                if (err) throw err;
+                console.log('user-request-logs saved successfully!');
+            });
+            break;
+        default:
+            console.log(req.params.buket + ' buket is Unknown.');
+            break;
+    }
+}
 
 module.exports = router;
