4f8510465176693dd12a8ae1d80c4b408d06da68
HJ.Park <hyunjun19@gmail.com>
2016-08-26 08:41:05 +0900

    express-ipfilter 추가 - ip를 white list로 관리

--- longo-app/app.js
+++ longo-app/app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var bodyParser   = require('body-parser');
+var mongoose     = require('mongoose');
+var ipfilter     = require('express-ipfilter')
 
 var routes = require('./routes/index');
 var logger = require('./routes/logger');
@@ -24,6 +25,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Whitelist the following IPs
+var ips = ['127.0.0.1'];
+
+app.use(ipfilter(ips, {mode: 'allow'}));
 
 app.use('/', routes);
 app.use('/logger', logger);
