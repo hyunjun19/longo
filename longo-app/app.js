124762901f10b426040c4572bb43bfff90713046
HJ.Park <hyunjun19@gmail.com>
2016-10-03 21:39:01 +0900

    bodyParser limit up to 100mb(default value 100kb)

--- longo-app/app.js
+++ longo-app/app.js
@@ -21,8 +21,8 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '100mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
